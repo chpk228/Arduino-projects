1682bb20a562246e895a1085eb0630d7174d2651
Pavan Ch <pk7057764@gmail.com>
2025-10-10 05:20:37 +0530

    file are added

---
+++ general/servomotors/servorev.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myServo;
+int pos = 0;
+
+void setup() {
+  myServo.attach(9);
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos++) {
+    myServo.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0; pos--) {
+    myServo.write(pos);
+    delay(15);
+  }
+}
+
