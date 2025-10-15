79544201361173031de53ec3b12baa75af856156
Pavan Ch <pk7057764@gmail.com>
2025-10-15 06:36:30 +0530

    file are added

---
+++ general/steppermotors/pwm-input.ino
@@ -0,0 +1,19 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200;
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  myStepper.setSpeed(60);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
