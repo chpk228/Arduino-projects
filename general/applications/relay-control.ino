c978eb5037998f3e0e11c8503e3613a3543e6d59
Pavan Ch <pk7057764@gmail.com>
2025-10-16 20:02:43 +0530

    file are add

---
+++ general/applications/relay-control.ino
@@ -0,0 +1,29 @@
+const int zero_cross_pin = 2;
+const int ssr_control_pin = 3;
+volatile unsigned long zero_cross_micros = 0;
+const int half_period_micros = 10000;
+const int ssr_delay = 3000;
+
+void zero_cross_interrupt() {
+  zero_cross_micros = micros();
+}
+
+void setup() {
+  pinMode(zero_cross_pin, INPUT);
+  pinMode(ssr_control_pin, OUTPUT);
+  digitalWrite(ssr_control_pin, LOW);
+  attachInterrupt(digitalPinToInterrupt(zero_cross_pin), zero_cross_interrupt, RISING);
+}
+
+void loop() {
+  if (micros() - zero_cross_micros < half_period_micros) {
+    if (micros() - zero_cross_micros >= ssr_delay) {
+      digitalWrite(ssr_control_pin, HIGH);
+    } else {
+      digitalWrite(ssr_control_pin, LOW);
+    }
+  } else {
+    digitalWrite(ssr_control_pin, LOW);
+  }
+}
+
